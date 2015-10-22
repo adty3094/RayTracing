50cdeb65ca586c0e9b2a7033a20d2c5f81db2630
o2zephyr <yohanes.aditya94@gmail.com>
2015-10-22 11:03:03 +0700

    init commit

---
+++ RayTracing/Camera.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracing
+{
+    class Camera
+    {
+        private Vector lookFrom; 
+        private Vector lookAt;
+        private Vector up;
+        private double fov;
+
+        private Vector u, v, w;
+
+        public Camera(double LookFromX, double LookFromY, double LookFromZ,
+               double LookAtX, double LookAtY, double LookAtZ,
+               double UpX, double UpY, double UpZ, double fov)
+        {
+            this.lookFrom = new Vector(LookFromX, LookFromY, LookFromZ);
+            this.lookAt = new Vector(LookAtX, LookAtY, LookAtZ);
+            this.up = new Vector(UpX, UpY, UpZ);
+        }
+
+        public void CalculateUVW()
+        {
+
+        }
+    }
+}
---
+++ RayTracing/Vector.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracing
+{
+    class Vector
+    {
+        double x, y, z;
+        double normalize;
+
+        public Vector()
+        {
+
+        }
+        
+        public Vector(double x, double y, double z)
+        {
+            SetVector(x, y, z);
+        }
+
+        public void SetVector(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public void NormalizeCalculation()
+        {
+            normalize = Math.Sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
+        }
+
+        public double DotProduct(Vector u, Vector v)
+        {
+            return u.x * v.x + u.y * v.y + u.z * v.z;
+        }
+
+        public Vector CrossProduct(Vector u, Vector v)
+        {
+            Vector crossProduct = new Vector(u.y * v.z - u.z * v.y,
+                                             u.z * v.x - u.x * v.z,
+                                             u.x * v.y - u.y * v.x);
+            return crossProduct;
+        }
+    }
+}
